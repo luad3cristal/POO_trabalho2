b4f51e79340d0664991ae55ad52de95f2a300d3a
Luana Lima <luanalima.2006@hotmail.com>
2025-07-23 23:33:03 -0300

    feat: add initial database connection logic

--- app/src/main/java/br/edu/ifba/inf008/App.java
+++ app/src/main/java/br/edu/ifba/inf008/App.java
@@ -1,12 +1,19 @@
 package br.edu.ifba.inf008;
 
-import br.edu.ifba.inf008.shell.Core;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        Core.init();
+        try (Connection conn = DBConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM users")) {
+            while (rs.next()) {
+                System.out.println("User: " + rs.getString("name"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
