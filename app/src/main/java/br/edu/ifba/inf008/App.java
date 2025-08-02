407335a07d0bd4225f10067145040a0ff22ab2a6
Luana Lima <luanalima.2006@hotmail.com>
2025-08-02 03:00:26 -0300

    feat: restore app.java

--- app/src/main/java/br/edu/ifba/inf008/App.java
+++ app/src/main/java/br/edu/ifba/inf008/App.java
@@ -1,19 +1,12 @@
 package br.edu.ifba.inf008;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import br.edu.ifba.inf008.shell.Core;
 
+/**
+ * Hello world!
+ */
 public class App {
     public static void main(String[] args) {
-        try (Connection conn = DBConnection.getConnection();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT * FROM users")) {
-            while (rs.next()) {
-                System.out.println("User: " + rs.getString("name"));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Core.init();
     }
 }
